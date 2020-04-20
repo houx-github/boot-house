06483c160e4207b0e33916962a41e37ee835aef9
houxin <1587466761@qq.com>
2020-04-20 17:50:47 +0800

    配置swagger

--- src/main/java/com/etoak/controller/AreaController.java
+++ src/main/java/com/etoak/controller/AreaController.java
@@ -2,6 +2,9 @@ package com.etoak.controller;
 
 import com.etoak.bean.Area;
 import com.etoak.service.AreaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/area")
 @Slf4j
+
 public class AreaController {
     @Autowired
     AreaService areaService;
@@ -23,6 +27,9 @@ public class AreaController {
      * pid为0表示查询省列表
      */
     @GetMapping("/queryByPid")
+    @ApiOperation(value = "查询地区根据父id",notes = "查询地区根据父id")
+    @ApiImplicitParam(value = "父id",name = "pid",
+            required = false,defaultValue = "0",dataType = "int",paramType = "query")
     public List<Area> queryByPid(@RequestParam(required = false,defaultValue = "0")
                                  int pid){
        log.info("pid - {}",pid);
