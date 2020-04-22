dcc23564d5a91a2a29a95ef783fe0f062a6eae9f
houxin <1587466761@qq.com>
2020-04-22 17:16:35 +0800

    完成部分查询列表接口调用

--- src/main/java/com/etoak/controller/HouseController.java
+++ src/main/java/com/etoak/controller/HouseController.java
@@ -1,8 +1,11 @@
 package com.etoak.controller;
 
 import com.etoak.bean.House;
+import com.etoak.bean.HouseVo;
+import com.etoak.bean.Page;
 import com.etoak.service.HouseService;
 import com.etoak.utils.ValidationUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -23,6 +24,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 @RequestMapping("/house")
 public class HouseController {
     @Value("${upload.dir}")
@@ -95,5 +97,12 @@ public class HouseController {
       houseService.addHouse(house);
       return "redirect:/house/toAdd";
   }
-
+    @GetMapping(value = "/list",produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public Page<HouseVo> queryList(@RequestParam(required = false,defaultValue = "1") int pageNum,
+                                   @RequestParam(required = false,defaultValue = "10") int pageSize,
+                                   HouseVo houseVo){
+log.info("pageNum - {},pageSize - {},hoseVo - {}",pageNum,pageSize,houseVo);
+return houseService.queryList(pageNum,pageSize,houseVo);
+    }
 }
--- src/main/java/com/etoak/service/HouseService.java
+++ src/main/java/com/etoak/service/HouseService.java
@@ -1,7 +1,20 @@
 package com.etoak.service;
 
 import com.etoak.bean.House;
+import com.etoak.bean.HouseVo;
+import com.etoak.bean.Page;
+
+import java.util.List;
 
 public interface HouseService {
     int addHouse(House house);
+
+    /**
+     * 条件列表查询
+     * @param pageNum
+     * @param pageSize
+     * @param houseVo
+     * @return
+     */
+    Page<HouseVo> queryList(int pageNum,int pageSize,HouseVo houseVo);
 }
